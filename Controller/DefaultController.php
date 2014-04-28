1d4e836c4a9be42d8f2deb2092cea6933dfde1cb
Neonunux <regis.leloup@colombbus.org>
2014-04-28 11:14:03 +0200

    routing, controller, View creation,

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function svgEditorAction()
     {
-        return $this->render('TangaraSVGEditorBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('TangaraSVGEditorBundle:Default:svg-editor.html.twig');
     }
 }
