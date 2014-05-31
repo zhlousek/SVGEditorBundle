4dcec3e7f437e2b3b5f9ab350140139db7dea476
Neonunux <regis.leloup@colombbus.org>
2014-05-31 19:54:49 +0200

    Test removal

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -2,16 +2,10 @@
 
 namespace Tangara\SVGEditorBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class DefaultControllerTest extends WebTestCase
+class PhpunitTest extends \PHPUnit_Framework_TestCase
 {
-    public function testIndex()
+    public function testPhpunit()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue(true);
     }
 }
